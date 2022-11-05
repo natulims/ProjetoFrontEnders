8c286ea6996a76518308c5ceab1f682f0b314e2a
Camila Oliveira <114073795+Cxmxla@users.noreply.github.com>
2022-11-05 17:43:13 -0300

    adição de responsividade no cadastro, login, etc

--- assets/js/cadastro/cadastro.js
+++ assets/js/cadastro/cadastro.js
@@ -1,23 +1,34 @@
+/*cadastro*/
 const nome = document.getElementById("name");
 const sobrenome = document.getElementById("surname");
 const email = document.getElementById("email");
 const rg = document.getElementById("id");
-const cep = document.getElementById("cep")
-const rua = document.getElementById ("rua");
-const bairro = document.getElementById("bairro")
-const cidade = document.getElementById("cidade")
-const estado = document.getElementById("uf")
-const numero = document.getElementById("numero")
-const complemento = document.getElementById("complemento")
+const cep = document.getElementById("cep");
+const rua = document.getElementById("rua");
+const bairro = document.getElementById("bairro");
+const cidade = document.getElementById("cidade");
+const estado = document.getElementById("uf");
+const numero = document.getElementById("numero");
+const complemento = document.getElementById("complemento");
 
-function cadastro (){
-    if (nome.value == '' || sobrenome.value == '' || email.value == '' || rg.value == '' ||
-    cep.value == '' || rua.value == '' || bairro.value == '' || cidade.value == '' || estado.value == '' ||
-    numero.value == '' || complemento.value == ''){
-        alert ("Preencha todos os campos.")
-    }
+/*função*/
 
-    else{
-        alert ("Cadastro realizado.")
-    }
+function cadastro() {
+  if (
+    nome.value == "" ||
+    sobrenome.value == "" ||
+    email.value == "" ||
+    rg.value == "" ||
+    cep.value == "" ||
+    rua.value == "" ||
+    bairro.value == "" ||
+    cidade.value == "" ||
+    estado.value == "" ||
+    numero.value == "" ||
+    complemento.value == ""
+  ) {
+    alert("Preencha todos os campos.");
+  } else {
+    alert("Cadastro realizado.");
+  }
 }
--- assets/js/login/login.js
+++ assets/js/login/login.js
@@ -1,79 +1,66 @@
+/*login*/
 const email = document.getElementById("email");
 const error = document.querySelector(".error");
-const error2 = document.querySelector (".error2");
+const error2 = document.querySelector(".error2");
 const senha = document.getElementById("senha");
 const error3 = document.querySelector(".error3");
-const error4 = document.querySelector (".error4");
-const texto1 = document.querySelector(".texto1")
-const texto2 = document.querySelector(".texto2")
-const texto3 = document.querySelector(".texto3")
-const texto4 = document.querySelector(".texto4")
-const btn = document.querySelector(".btn")
+const error4 = document.querySelector(".error4");
+const texto1 = document.querySelector(".texto1");
+const texto2 = document.querySelector(".texto2");
+const texto3 = document.querySelector(".texto3");
+const texto4 = document.querySelector(".texto4");
+const btn = document.querySelector(".btn");
 
+/*funçao login*/
+function login() {
+  if (email.value.length == 0) {
+    error2.style.display = "initial";
+    email.style.borderColor = "red";
+    error.style.display = "none";
+  } else if (
+    email.value.indexOf("@") == -1 ||
+    (email.value.length < 13 && email.value.length > 0) ||
+    email.value.indexOf(".") == -1 ||
+    email.value.indexOf(".") - email.value.indexOf("@") == 1
+  ) {
+    error2.style.display = "none";
+    error.style.display = "initial";
+    email.style.borderColor = "red";
+  } else {
+    error2.style.display = "none";
+    error.style.display = "none";
+    email.style.borderColor = "gray";
+    var emails = true;
+  }
 
-function login(){
-    
+  if (senha.value.length == 0) {
+    senha.style.borderColor = "red";
+    error4.style.display = "initial";
+    error3.style.display = "none";
+  } else if (senha.value.length < 8) {
+    senha.style.borderColor = "red";
+    error3.style.display = "initial";
+    error4.style.display = "none";
+  } else {
+    senha.style.borderColor = "gray";
+    error4.style.display = "none";
+    error3.style.display = "none";
+    var senhas = true;
+  }
 
-
-    if (email.value.length == 0){
-        error2.style.display = "initial"
-        email.style.borderColor = "red";
-        error.style.display = "none";
-    }
-
-    else if (email.value.indexOf("@")== -1 || email.value.length < 13 && email.value.length > 0 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)){
-        error2.style.display = "none"
-        error.style.display = "initial";
-        email.style.borderColor = "red";
-    }
-    
-
-    else {
-        error2.style.display = "none";
-        error.style.display = "none";
-        email.style.borderColor = "gray";
-        var emails = true;
-        
-    }
-    
-    
-    if (senha.value.length == 0){
-        senha.style.borderColor = "red";
-        error4.style.display = "initial";
-        error3.style.display = "none";
-    }
-
-    else if (senha.value.length < 8){
-        senha.style.borderColor = "red";
-        error3.style.display = "initial";
-        error4.style.display = "none";
-    }
-
-    else{
-        senha.style.borderColor = "gray"
-        error4.style.display = "none";
-        error3.style.display = "none";
-        var senhas = true;
-    }
-
-    if (senhas == true && emails == true)
-    {
-        texto1.textContent = "Login efetuado com sucesso."
-        texto1.style.fontSize = "25pt"
-        email.style.display = "none"
-        error.style.display = "none"
-        error2.style.display = "none"
-        senha.style.display = "none"
-        error3.style.display = "none"
-        error4.style.display = "none"
-        texto2.style.display = "none"
-        btn.style.display = "none"
-        texto3.style.display = "none"
-        texto4.style.display = "none"
-    }
-
-    else{
-        
-    }
-
+  if (senhas == true && emails == true) {
+    texto1.textContent = "Login efetuado com sucesso.";
+    texto1.style.fontSize = "25pt";
+    email.style.display = "none";
+    error.style.display = "none";
+    error2.style.display = "none";
+    senha.style.display = "none";
+    error3.style.display = "none";
+    error4.style.display = "none";
+    texto2.style.display = "none";
+    btn.style.display = "none";
+    texto3.style.display = "none";
+    texto4.style.display = "none";
+  } else {
+  }
 }
